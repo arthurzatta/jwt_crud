bb7ff29a8cadc0a14dcf9ddb85be4009de016f3a
arthurzatta <arthur.zatta@gmail.com>
2021-04-16 15:36:15 -0300

    Initial controller done

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -8,12 +8,34 @@ class User{
             hashPassword
         });
 
-        return res.status(400).end();
+        return res.status(200).end();
     }
 
     async list(req,res){
         const query = await knex().select().table('users');
         return res.json(query);
+    }
+
+    async delete(req, res){
+        const { id } = req.params;
+        await knex('users').where('id',id).del();
+        return res.status(200).end();
+    }
+
+    //Update needs to be done in the user section
+    //users doesn't need to pass your ids to update your datas
+    //so this why JWT is usefull
+    async update(req, res){
+        const { id } = req.params;
+        const { name } = req.body;
+
+        if(!name) {
+            return res.status(400).end();
+        }
+
+        await knex('users').where("id", id).update({ name });
+        
+        return res.status(200).end()
     }
 
 }
